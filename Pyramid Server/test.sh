e83cde18e3fbfdce5fa90d26f1fad73e0d9bc36e
pwnwithlove <pwnwithlove@debian-BULLSEYE-live-builder-AMD64>
2023-04-19 21:37:01 +0200

    Adding SSTI properly !

---
+++ Pyramid Server/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function setup_fun {
+    export VENV=~/env
+    echo "[+] Creation of the virtual environment.." && python3 -m venv $VENV
+    echo "[+] Installation of the project.. " && $VENV/bin/pip install -e . > /dev/null
+    echo "[+] Traceback mode ? [y] or [n]"
+    read choix
+    if [ "$choix" = "y" ]; then
+        $VENV/bin/pserve development.ini --reload | echo "[+] Done ! Project available here: http://127.0.0.1:6543"
+    elif [ "$choix" = "n" ]; then
+        echo "[+] Done ! Project available here: http://127.0.0.1:6543"
+        $VENV/bin/pserve development.ini > /dev/null 2>&1
+    else
+        echo "[+] Invalid !"
+    fi
+}
+
+setup_fun
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#just to setup automatically the environnement cuz i'm lazy 
+
+function setup_fun {
+    cd "./Pyramid Server"
+    export VENV=~/env
+    echo "[+] Creation of the virtual environment.." && python3 -m venv $VENV
+    echo "[+] Installation of the project.. " && $VENV/bin/pip install -e . > /dev/null
+    echo "[+] Traceback mode ? [y] or [n]"
+    read choix
+    if [ "$choix" = "y" ]; then
+        $VENV/bin/pserve development.ini --reload | echo "[+] Done ! Project available here: http://127.0.0.1:6543"
+    elif [ "$choix" = "n" ]; then
+        echo "[+] Done ! Project available here: http://127.0.0.1:6543"
+        $VENV/bin/pserve development.ini > /dev/null 2>&1
+    else
+        echo "[+] Invalid !"
+    fi
+}
+
+setup_fun
