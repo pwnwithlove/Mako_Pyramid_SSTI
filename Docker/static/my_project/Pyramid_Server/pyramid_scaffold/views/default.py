4887a7306416b5e239a8181167a3dcda84119f5a
pwnwithlove <pwnwithlove@debian-BULLSEYE-live-builder-AMD64>
2023-05-15 00:19:31 +0200

    Sanitise {{user_login}}

--- Docker/static/my_project/Pyramid_Server/pyramid_scaffold/views/default.py
+++ Docker/static/my_project/Pyramid_Server/pyramid_scaffold/views/default.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 from mako.template import Template
+import re
 
 
 @view_config(route_name='index', renderer='pyramid_scaffold:templates/index.mako')
@@ -12,9 +13,10 @@ def myaccount(request):
     if 'user_login' in request.params:
         path_to_html_file_account = "pyramid_scaffold/templates/invalid-username.mako"
         html_file_account = open(path_to_html_file_account,"r").read()
-        data = html_file_account.replace("{{user_login}}",request.params['user_login'])
+        user_login = re.sub("os", "", request.params["user_login"]) #sanitise
+        data = html_file_account.replace("{{user_login}}",user_login) 
         template = Template(data) 
-        return Response(template.render())
+        return Response(template.render()) 
     else:
         template = Template(filename="pyramid_scaffold/templates/my-account.mako")
         return Response(template.render())
@@ -22,11 +24,3 @@ def myaccount(request):
 @view_config(route_name='invalidusername', renderer='pyramid_scaffold:templates/invalid-username.mako')
 def invalidusername(request):
     return {'project': 'Pyramid Scaffold'}
-
-
-#def myaccount(request):
-    #if 'user_login' in request.params:
-        #template = Template(filename="pyramid_scaffold/templates/my-account.mako" & request.params['user_login']) #Création de la template
-        #return Response(template.render())
-    #else: 
-        #"return Response(template.render())
